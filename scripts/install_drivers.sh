1606cda4c548974d611eeb12d0cb28b6035cee5f
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-06 02:44:01 +0000

    fixed install script

--- scripts/install_drivers.sh
+++ scripts/install_drivers.sh
@@ -1,10 +1,30 @@
-wget https://github.com/ykhan1999/zero2w_80211ah/releases/download/v0.0.1/hostapd.deb
-wget https://github.com/ykhan1999/zero2w_80211ah/releases/download/v0.0.1/morse_cli.deb
-wget https://github.com/ykhan1999/zero2w_80211ah/releases/download/v0.0.1/wpa_supplicant.deb
-wget https://github.com/ykhan1999/zero2w_80211ah/releases/download/v0.0.1/morse_firmware.deb
+#!/usr/bin/env bash
+set -euo pipefail
+
+### Download assets
+#get latest release
+REPO="ykhan1999/zero2w_80211ah"
+BASE="https://github.com/ykhan1999/zero2w_80211ah/releases/latest/download"
+curl -Ls -o /dev/null -w %{url_effective} "${BASE}" > /tmp/url
+URL=$(cat "/tmp/url")
+
+# Download assets from the latest release (filenames must be consistent across releases)
+assets=(
+  hostapd.deb
+  morse_cli.deb
+  wpa_supplicant.deb
+  morse_firmware.deb
+)
+
+for a in "${assets[@]}"; do
+  echo "Downloading ${a} from latest release…"
+  wget "${URL}/${a}"
+done
+
 sudo apt-get update
-sudo apt install -y ./hostapd.deb
-sudo apt install -y ./morse_cli.deb
-sudo apt install -y ./wpa_supplicant.deb
-sudo apt install -y ./morse_firmware.deb
+for a in "${assets[@]}"; do
+  echo "Installing ${a} from latest release…"
+  sudo apt install -y ./${a}
+done
+
 sudo depmod -a
--- scripts/install_kernel.sh
+++ scripts/install_kernel.sh
@@ -1,3 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+### Download assets
+#get latest release
+REPO="ykhan1999/zero2w_80211ah"
+BASE="https://github.com/ykhan1999/zero2w_80211ah/releases/latest/download"
+curl -Ls -o /dev/null -w %{url_effective} "${BASE}" > /tmp/url
+URL=$(cat "/tmp/url")
+
+
+# Download assets from the latest release (filenames must be consistent across releases)
+assets=(
+  rpilinux-zero2w_1.0-2.deb
+)
+
+for a in "${assets[@]}"; do
+  echo "Downloading ${a} from latest release…"
+  wget "${URL}/${a}"
+done
+
+
+#Manual install
+echo "Installing ${a}"
 sudo mkdir -p /tmp/rpi-fix/
 wget https://github.com/ykhan1999/zero2w_80211ah/releases/download/v0.0.1/rpilinux-zero2w_1.0-2.deb
 sudo dpkg -x rpilinux-zero2w_1.0-2.deb /tmp/rpi-fix/
