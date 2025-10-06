a04df4b4d32c25e0292b2aea0b5777764f1c61c6
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-06 03:08:21 +0000

    fix spi instal script

--- scripts/install_SPI_overlays.sh
+++ scripts/install_SPI_overlays.sh
@@ -6,5 +6,5 @@ sudo apt-get update
 sudo apt-get install device-tree-compiler
 dtc -@ -I dts -O dtb -o $SCRIPT_DIR/../overlays/morse-spi.dtbo $SCRIPT_DIR/../overlays/morse-spi.dts
 dtc -@ -I dts -O dtb -o $SCRIPT_DIR/../overlays/mm-wlan.dtbo $SCRIPT_DIR/../overlays/mm-wlan.dts
-sudo cp $SCRIPT_DIR/overlays/*.dtbo /boot/firmware/overlays/
+sudo cp $SCRIPT_DIR/../overlays/*.dtbo /boot/firmware/overlays/
 sudo reboot
