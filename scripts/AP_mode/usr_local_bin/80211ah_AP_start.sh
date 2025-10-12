b3f3381fc9afeb5d73775c34a9065cd607fc48f2
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-12 02:10:36 +0000

    more bug fixes yet

--- scripts/AP_mode/usr_local_bin/80211ah_AP_start.sh
+++ scripts/AP_mode/usr_local_bin/80211ah_AP_start.sh
@@ -14,5 +14,22 @@ ip link set wlan1 up
 #enable NAT forward and DHCP server for wlan1
 /usr/local/bin/toggle_NAT.sh --on
 
+#wait before bringing up AP
+sleep 1
+
 #bring up AP
 hostapd_s1g -t /usr/local/etc/hostapd.conf
+
+#if not running after 10s, try to bring up again
+while true; do
+  sleep 10
+  PID=$(pgrep -x hostapd_s1g)
+
+  if [[ -n "$PID" ]]; then
+    echo "hostapd_s1g is running with PID $PID"
+    break
+  else
+    echo "hostapd_s1g not yet running... retrying in 10s"
+  fi
+done
+
