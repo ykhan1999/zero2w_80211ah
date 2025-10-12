4206f33050900a5bd422804b479314c6a5311c21
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-12 01:54:58 +0000

    more bug fixes

--- scripts/AP_mode/usr_local_bin/80211ah_AP_stop.sh
+++ scripts/AP_mode/usr_local_bin/80211ah_AP_stop.sh
@@ -2,7 +2,7 @@
 
 #Stop hostapd, try gracefully
 PID=$(pgrep -x hostapd_s1g)
-sudo kill -TERM "$PID"
+kill -TERM "$PID"
 
 for i in {1..10}; do
   if ! ps -p "$PID" > /dev/null; then
@@ -14,7 +14,7 @@ done
 
 if ps -p "$PID" > /dev/null; then
   echo "Graceful shutdown timed out. Forcing termination..."
-  sudo kill -KILL "$PID"
+  kill -KILL "$PID"
   sleep 1
 fi
 
@@ -22,6 +22,6 @@ fi
 /usr/local/bin/toggle_NAT.sh --off
 
 #remove static ip
-sudo ip link set wlan1 down
-sudo ip addr flush dev wlan1
-sudo ip link set wlan1 up
+ip link set wlan1 down
+ip addr flush dev wlan1
+ip link set wlan1 up
--- scripts/AP_mode/usr_local_bin/toggle_NAT.sh
+++ scripts/AP_mode/usr_local_bin/toggle_NAT.sh
@@ -33,36 +33,36 @@ fi
 if [[ "$MODE" == "on" ]]; then
   echo "Turning ON forwarding with WAN=$WAN and LAN=$LAN"
   #apply nftables ruleset
-  sudo cp /usr/local/etc/nftables_forward.conf.disabled /etc/nftables.conf
+  cp /usr/local/etc/nftables_forward.conf.disabled /etc/nftables.conf
   #restart nftables if active, if inactive then start
   enabled="$(systemctl is-enabled nftables)"
   if [[ "$enabled" != "enabled" ]]; then
-     sudo systemctl enable --now nftables
+     systemctl enable --now nftables
   else
-     sudo systemctl restart nftables
+     systemctl restart nftables
   fi
   #Start DHCP server
-  sudo cp /usr/local/etc/dnsmasq_DHCP.conf.disabled /etc/dnsmasq.d/lan-$LAN.conf
-  sudo systemctl restart dnsmasq
+  cp /usr/local/etc/dnsmasq_DHCP.conf.disabled /etc/dnsmasq.d/lan-$LAN.conf
+  systemctl restart dnsmasq
   echo "DHCP server enabled"
   #keep networkmanager away from $LAN
-  sudo cp /usr/local/etc/netman_unmanaged.conf.disabled /etc/NetworkManager/conf.d/unmanaged.conf
-  sudo systemctl restart NetworkManager
+  cp /usr/local/etc/netman_unmanaged.conf.disabled /etc/NetworkManager/conf.d/unmanaged.conf
+  systemctl restart NetworkManager
   echo "NAT forwarding ENABLED on ${LAN} -> ${WAN}"
 
 #Turn off NAT forwarding
 elif [[ "$MODE" == "off" ]]; then
   echo "Turning OFF all forwarding"
   #apply default nftables ruleset
-  sudo cp /usr/local/etc/nftables_noforward.conf.disabled /etc/nftables.conf
-  sudo systemctl restart nftables
+  cp /usr/local/etc/nftables_noforward.conf.disabled /etc/nftables.conf
+  systemctl restart nftables
   #turn off DHCP
-  sudo rm -r /etc/dnsmasq.d/lan-$LAN.conf
-  sudo systemctl stop dnsmasq
+  rm -r /etc/dnsmasq.d/lan-$LAN.conf
+  systemctl stop dnsmasq
   echo "DHCP server disabled"
   #give network manager control again
-  sudo rm -r /etc/NetworkManager/conf.d/unmanaged.conf
-  sudo systemctl restart NetworkManager
+  rm -r /etc/NetworkManager/conf.d/unmanaged.conf
+  systemctl restart NetworkManager
   echo "NAT forwarding DISABLED for ${LAN}"
 
 else
