7a57cffe3b4191a7fa0c7de9a5c4d6767369d262
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-13 03:15:08 +0000

    mesh fixes

--- scripts/mesh_mode/usr_local_bin/batman_stop.sh
+++ scripts/mesh_mode/usr_local_bin/batman_stop.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-FILE="/usr/local/etc/batman_gateway_status.txt"
-
-#deactivate gateway if active
-if [[ -f "$FILE" ]] && grep -q "gateway=active" "$FILE"; then
-    echo "Gateway is active. Turning off NAT forwarding and DHCP server..."
-    /usr/local/bin/toggle_NAT_batman.sh --off
-    rm -r /usr/local/etc/batman_gateway_status.txt
-fi
-
 ###for gateway or client
-
 #remove bat0 interface
 batctl if del wlan1
 batctl meshif bat0 interface destroy
@@ -25,3 +15,12 @@ ip link set wlan1 up
 
 #unload batman driver
 modprobe -r batman-adv
+
+###for gateway
+#deactivate NAT forwarding
+FILE="/usr/local/etc/batman_gateway_status.txt"
+if [[ -f "$FILE" ]] && grep -q "gateway=active" "$FILE"; then
+    echo "Gateway is active. Turning off NAT forwarding and DHCP server..."
+    /usr/local/bin/toggle_NAT_batman.sh --off
+    rm -r /usr/local/etc/batman_gateway_status.txt
+fi
