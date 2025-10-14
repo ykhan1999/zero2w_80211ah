487289c218fdd0306d581f0644c55a7f472af165
ykhan1999 <yousef.khan@pennmedicine.upenn.edu>
2025-10-14 03:35:15 +0000

    fixed install script

--- scripts/init/install_drivers.sh
+++ scripts/init/install_drivers.sh
@@ -12,7 +12,6 @@ URL=$(cat "/tmp/url")
 assets=(
   hostapd.deb
   morse_cli.deb
-  wpa_supplicant.deb
   morse_firmware.deb
 )
 
@@ -25,6 +24,23 @@ sudo apt-get update
 for a in "${assets[@]}"; do
   echo "Installing ${a} from latest release…"
   sudo apt install -y ./${a}
+done
+
+#for some reason, wpa_supplicant_s1g doesn't play nice with package manager
+
+assets=(
+  wpa_supplicant.deb
+)
+
+for a in "${assets[@]}"; do
+  echo "Downloading ${a} from latest release…"
+  wget "${URL}/${a}"
+done
+
+sudo apt-get update
+for a in "${assets[@]}"; do
+  echo "Installing ${a} from latest release…"
+  sudo dpkg -i ./${a}
 done
 
 sudo depmod -a
