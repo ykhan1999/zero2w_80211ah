a58e4c08b906004b00c4e005022d9495592dacdd
ykhan1999 <yousefkhan125@gmail.com>
2025-11-20 19:31:18 -0500

    harden disable script

--- scripts/mesh_80211s/disable_mesh.sh
+++ scripts/mesh_80211s/disable_mesh.sh
@@ -1,10 +1,7 @@
-#if running as gateway
-if grep "active" /usr/local/etc/80211s_gateway_status.txt; then
-    sudo systemctl stop 80211s_gateway.service
-    sudo systemctl disable 80211s_gateway.service
-    sudo pkill -f "python3 -m http.server" 2>/dev/null || true
-#if client
-else
-sudo systemctl stop 80211s_client.service || true
-sudo systemctl disable 80211s_client.service || true
-fi
+#for gateway
+sudo systemctl stop 80211s_gateway.service 2>/dev/null || true
+sudo systemctl disable 80211s_gateway.service 2>/dev/null || true
+sudo pkill -f "python3 -m http.server" 2>/dev/null || true
+#for client
+sudo systemctl stop 80211s_client.service 2>/dev/null || true
+sudo systemctl disable 80211s_client.service 2>/dev/null || true
