45d135674d33da75ae58bfe704d271523338b3fd
ykhan1999 <yousefkhan125@gmail.com>
2025-12-03 00:15:17 -0500

    progress on bluetooth daemon, added ticker for rssi

--- bluetooth/init.sh
+++ bluetooth/init.sh
@@ -10,6 +10,9 @@
 #cp -r build/lib.linux-aarch64-cpython-313/* ..
 #cd ..
 
+sudo cp ble_server.py /usr/local/bin/
+sudo cp btfpy.cpython-313-aarch64-linux-gnu.so /usr/local/bin/
+sudo cp devices.txt /usr/local/bin/
 sudo cp services/ble_server.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now ble_server
