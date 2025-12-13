3f8fd4a5925cf5012572ad769b527a5563168c94
ykhan1999 <yousefkhan125@gmail.com>
2025-12-12 23:30:53 -0500

    add init script for webserver config

---
+++ scripts/init/install_webserver.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo apt install -y npm
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${SCRIPT_DIR}/../../webserver/frontend/
+npm install
+npm run build
+cd -
