0d303c3db2b7c3a3d5cbf1192dffdf647dcac6bc
ykhan1999 <yousefkhan125@gmail.com>
2025-12-19 05:55:50 -0500

    make the webserver functional

--- webserver/backend/scripts/activate_config.sh
+++ webserver/backend/scripts/activate_config.sh
@@ -22,10 +22,19 @@ done
 #enable config
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-${SCRIPT_DIR}/../../../scripts/mesh_80211s/config_mesh.sh --halow-ssid "${REG_SSID}" --halow-password "${REG_PW}"
+#same command to update config regardless of mode - hotspot ssid/pass won't do anything in gateway mode
+${SCRIPT_DIR}/../../../scripts/mesh_80211s/config_mesh.sh --hotspot-ssid "${REG_SSID}" --hotspot-password "${REG_PW}" --halow-ssid "${HALOW_SSID}" --halow-password "${HALOW_PW}"
 
-echo "$MODE"
-echo "$REG_SSID"
-echo "$REG_PW"
-echo "$HALOW_SSID"
-echo "$HALOW_PW"
+#now we differentiate
+if [ "$MODE" == "gateway" ]; then
+  #need to connect to gateway network
+  nmcli device wifi connect "${REG_SSID}" password "${REG_PW}" ifname wlan0
+  #only one script active at a time
+  ${SCRIPT_DIR}/../../../scripts/mesh_80211s/disable_mesh.sh
+  ${SCRIPT_DIR}/../../../scripts/mesh_80211s/enable_mesh_gateway.sh
+elif [ "$MODE" == "client" ]; then
+  ${SCRIPT_DIR}/../../../scripts/mesh_80211s/disable_mesh.sh
+  ${SCRIPT_DIR}/../../../scripts/mesh_80211s/enable_mesh_client.sh
+else
+  exit 1
+fi
