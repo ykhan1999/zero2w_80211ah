7bd62b1f86d149d993b3e5bb1552051cf79f8e62
ykhan1999 <yousefkhan125@gmail.com>
2025-12-21 12:56:06 -0500

    add webserver toggle

--- scripts/init/install_webserver.sh
+++ scripts/init/install_webserver.sh
@@ -15,6 +15,8 @@ sudo cp ${SCRIPT_DIR}/helpers/webserver-backend.service /etc/systemd/system/
 sudo cp ${SCRIPT_DIR}/helpers/webserver-frontend.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
+sudo cp ${SCRIPT_DIR}/helpers/toggle-webserver.sh /usr/local/bin
+sudo chmod +x /usr/local/bin/toggle-webserver.sh
 
 cd -
 cd -
