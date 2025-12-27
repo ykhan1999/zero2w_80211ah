92be4478ef752f676dfc09248d2de1316be66922
ykhan1999 <yousefkhan125@gmail.com>
2025-12-27 10:41:40 -0500

    make stop script agnostic and make sure disable mesh script runs

--- scripts/mesh_80211s/disable_mesh.sh
+++ scripts/mesh_80211s/disable_mesh.sh
@@ -1,3 +1,5 @@
+#run stop script
+/usr/local/bin/80211s_stop.sh
 #for gateway
 systemctl stop 80211s_gateway.service || true
 systemctl disable 80211s_gateway.service || true
--- scripts/mesh_80211s/usr_local_bin/80211s_stop.sh
+++ scripts/mesh_80211s/usr_local_bin/80211s_stop.sh
@@ -11,39 +11,36 @@ pkill -f 'wpa_supplicant_s1g'
 ###for gateway
 #deactivate NAT forwarding
 FILE="/usr/local/etc/80211s_gateway_status.txt"
-if [[ -f "$FILE" ]] && grep -q "gateway=active" "$FILE"; then
-    #stop serving DNS names
-    pkill -f "python3 -m http.server" 2>/dev/null
-    systemctl stop 80211s_serve_dns
-    systemctl disable 80211s_serve_dns
+#stop serving DNS names
+pkill -f "python3 -m http.server" || true
+systemctl stop 80211s_serve_dns || true
+systemctl disable 80211s_serve_dns || true
 
-    #disable NAT forwarding
-    echo "Gateway is active. Turning off NAT forwarding and DHCP server..."
-    /usr/local/bin/toggle_NAT_80211s.sh --off --gateway
+#disable NAT forwarding
+/usr/local/bin/toggle_NAT_80211s.sh --off --gateway || true
 
-    #remove static IP daemon and DHCP server
-    rm -r /etc/systemd/network/10-wlan1.network
-    systemctl restart systemd-networkd
+#remove static IP daemon and DHCP server
+rm -r /etc/systemd/network/10-wlan1.network || true
+systemctl restart systemd-networkd || true
 
-    #disable gateway flag
-    rm -r /usr/local/etc/80211s_gateway_status.txt
+#disable gateway flag
+rm -r /usr/local/etc/80211s_gateway_status.txt || true
 
-    #remove netman connection
-    nmcli connection down wifi-client-${ssid}
-    nmcli connection delete wifi-client-${ssid}
+#remove netman connection
+nmcli connection down wifi-client-${ssid} || true
+nmcli connection delete wifi-client-${ssid} || true
 
-    pkill -f "/usr/local/bin/disp_gateway_active.sh"
-else
+pkill -f "/usr/local/bin/disp_gateway_active.sh" || true
+
 ###for client mode
-    #turn off NAT forwarding
-    /usr/local/bin/toggle_NAT_80211s.sh --off --client
+#turn off NAT forwarding
+/usr/local/bin/toggle_NAT_80211s.sh --off --client || true
 
-    #remove netman connection
-    nmcli connection down wifi-ap-${ssid}
-    nmcli connection delete wifi-ap-${ssid}
+#remove netman connection
+nmcli connection down wifi-ap-${ssid} || true
+nmcli connection delete wifi-ap-${ssid} || true
 
-    pkill -f "/usr/local/bin/disp_client_active.sh"
-fi
+pkill -f "/usr/local/bin/disp_client_active.sh" || true
 
 #flush current IP
 ip link set wlan1 down
