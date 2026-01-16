f9590bba7678de6eb7091f555c64a2504b47738c
ykhan1999 <yousefkhan125@gmail.com>
2026-01-15 20:16:45 -0500

    speed up loading bar

--- display/scripts/disp_loading_bar.sh
+++ display/scripts/disp_loading_bar.sh
@@ -4,29 +4,16 @@ LINE=$1
 
 while true; do
   sudo disp_custom_msg.sh --line${LINE} "|***           |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "| ***          |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|  ***         |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|   ***        |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|    ***       |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|     ***      |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|      ***     |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|       ***    |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|        ***   |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|         ***  |"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|           ***|"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|*           **|"
-  sleep 0.5
   sudo disp_custom_msg.sh --line${LINE} "|**           *|"
-  sleep 0.5
 done
