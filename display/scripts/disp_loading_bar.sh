cde54f24ccdb8e7432f22f674d0c2a512ee9b215
ykhan1999 <yousefkhan125@gmail.com>
2026-01-15 20:38:55 -0500

    add sleep to prevent cpu overuse

--- display/scripts/disp_loading_bar.sh
+++ display/scripts/disp_loading_bar.sh
@@ -4,16 +4,29 @@ LINE=$1
 
 while true; do
   sudo disp_custom_msg.sh --line${LINE} "|***           |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "| ***          |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|  ***         |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|   ***        |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|    ***       |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|     ***      |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|      ***     |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|       ***    |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|        ***   |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|         ***  |"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|           ***|"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|*           **|"
+  sleep 0.1
   sudo disp_custom_msg.sh --line${LINE} "|**           *|"
+  sleep 0.1
 done
