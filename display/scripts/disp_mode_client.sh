63cd6bbe298e1cb41956c1d07e60f95575b564d5
ykhan1999 <yousefkhan125@gmail.com>
2026-01-20 00:02:27 -0500

    aesthetic updates

--- display/scripts/disp_mode_client.sh
+++ display/scripts/disp_mode_client.sh
@@ -2,6 +2,7 @@
 pkill -f "disp_loading_bar.sh" || true
 cd /usr/local/bin
 oled r
-oled +1 "Mode: Client"
+oled +1 "  Client Mode   "
+oled +U 1
 oled s
 cd -
--- display/scripts/disp_mode_gw.sh
+++ display/scripts/disp_mode_gw.sh
@@ -2,6 +2,7 @@
 pkill -f "disp_loading_bar.sh" || true
 cd /usr/local/bin
 oled r
-oled +1 "Mode: Gateway"
+oled +1 "  Gateway Mode  "
+oled +U 1
 oled s
 cd -
