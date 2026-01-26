0d7cd2caa97dbd483c0666f53fa8a943db4d41aa
ykhan1999 <yousefkhan125@gmail.com>
2026-01-25 22:06:08 -0500

    remove stray sudos

--- display/scripts/disp_loading_bar.sh
+++ display/scripts/disp_loading_bar.sh
@@ -3,30 +3,30 @@
 LINE=$1
 
 while true; do
-  sudo disp_custom_msg.sh --line${LINE} "|***           |"
+  disp_custom_msg.sh --line${LINE} "|***           |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "| ***          |"
+  disp_custom_msg.sh --line${LINE} "| ***          |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|  ***         |"
+  disp_custom_msg.sh --line${LINE} "|  ***         |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|   ***        |"
+  disp_custom_msg.sh --line${LINE} "|   ***        |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|    ***       |"
+  disp_custom_msg.sh --line${LINE} "|    ***       |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|     ***      |"
+  disp_custom_msg.sh --line${LINE} "|     ***      |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|      ***     |"
+  disp_custom_msg.sh --line${LINE} "|      ***     |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|       ***    |"
+  disp_custom_msg.sh --line${LINE} "|       ***    |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|        ***   |"
+  disp_custom_msg.sh --line${LINE} "|        ***   |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|         ***  |"
+  disp_custom_msg.sh --line${LINE} "|         ***  |"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|           ***|"
+  disp_custom_msg.sh --line${LINE} "|           ***|"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|*           **|"
+  disp_custom_msg.sh --line${LINE} "|*           **|"
   sleep 0.1
-  sudo disp_custom_msg.sh --line${LINE} "|**           *|"
+  disp_custom_msg.sh --line${LINE} "|**           *|"
   sleep 0.1
 done
